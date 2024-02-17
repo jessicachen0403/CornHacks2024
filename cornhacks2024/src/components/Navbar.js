4e860bbaf8a7e37ede098f71c8d161d1c7a0ad5a
Madison Maloney <mmaloney3@huskers.unl.edu>
2024-02-17 17:41:00 -0600

    updated navbar

--- cornhacks2024/src/components/Navbar.js
+++ cornhacks2024/src/components/Navbar.js
@@ -1,15 +1,22 @@
-import { Link } from "react-router-dom";
-import '../css/navbar.css';
+import React from 'react';
+import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import {Link as ReactRouterLink} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1>Live Study Buddy</h1>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/about/">About</Link>
-      </div>
-    </nav>
+    <Box bg="#DFD5A5" color="#628395" p={4} top="0" left="0" right="0" zIndex="1000" width="100%">
+      <Flex justify="space-between" align="center">
+        <Text fontSize="xl">Live Study Buddy</Text>
+        <Flex>
+          <Link fontSize="xl" as={ReactRouterLink} to="/" mx={2}>
+            Home
+          </Link>
+          <Link fontSize="xl" as={ReactRouterLink} to="/about" mx={2}>
+            About
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
  
